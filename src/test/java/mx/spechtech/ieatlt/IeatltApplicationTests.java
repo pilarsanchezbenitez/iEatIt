6acbddf7dcbb4d760be0563f2bb29e9adcd1d385
Raúl González <gonzalv@ciencias.unam.mx>
2020-04-04 22:25:50 -0600

    First commit

---
+++ src/test/java/mx/spechtech/ieatlt/IeatltApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.spechtech.ieatlt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeatltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
